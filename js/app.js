0a30ebf0b03d6d704b47b26bd134aa86c8b6e830
Keith Underwood <kuwood@users.noreply.github.com>
2016-06-26 21:22:16 -0600

    Add animations

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 function showDefine() {
   var define = "<p class='define'>I am <strong>Web Developer</strong> who is interested in technology, music, sports, games, outdoors, beer and discovering new things. I pride myself in <strong>self discipline</strong> and my thirst for <strong>efficiency</strong>. I also love <strong>sharing experiences</strong>, laughs, and smiles.</p>"
-  $('.main-content .content-wrap').html(define)
+  $('.main-content .content-wrap').hide().html(define).fadeIn()
   $('#0').css({'font-size':'1.4em'})
   $('.main-content').css({"margin-left":"5%"})
 }
 
 function showProjects() {
   var projects = '<div class="project"><img src="img/flowers.jpg"></div><div class="project"><img src="img/flowers.jpg"></div><div class="project"><img src="img/flowers.jpg"></div><div class="project"><img src="img/flowers.jpg"></div>';
-  $('.main-content .content-wrap').html(projects)
+  $('.main-content .content-wrap').hide().html(projects).fadeIn()
   $('.main-content').css({"margin-left":"5%"})
 }
 
 function showContact() {
   var contact = '<form class="contact"><input id="name-text" type="text" name="name" value="" placeholder="Full name">    <input id="subject-text" type="text" name="name" value="" placeholder="Subject"><textarea id="body-text" name="name" rows="8" cols="40" placeholder="Your message"></textarea><input class="btn" onclick="sendMail(); return false" type="button" name="name" value="Send"></form>';
-  $('.main-content .content-wrap').html(contact)
+  $('.main-content .content-wrap').hide().html(contact).fadeIn()
   $('.main-content').css({"margin-left":"0"})
 }
 
@@ -31,16 +31,16 @@ function createNav() {
 
 function navRouter(i) {
   if(i.text() === "Keith Underwood") {
-    $('.nav-link').css({'font-size':'1.2em'});
-    $('#0').css({'font-size':'1.4em'})
+    $('.nav-link').animate({'font-size':'1.2em'}, 50);
+    $('#0').animate({'font-size':'1.4em'}, 100)
     showDefine();
   } else if (i.text() === "What I've done") {
-    $('.nav-link').css({'font-size':'1.2em'});
-    $('#1').css({'font-size':'1.4em'})
+    $('.nav-link').animate({'font-size':'1.2em'}, 50);
+    $('#1').animate({'font-size':'1.4em'}, 100)
     showProjects();
   } else {
-    $('.nav-link').css({'font-size':'1.2em'});
-    $('#2').css({'font-size':'1.4em'})
+    $('.nav-link').animate({'font-size':'1.2em'}, 50);
+    $('#2').animate({'font-size':'1.4em'}, 100)
     showContact();
   };
 };
