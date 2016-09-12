c5cdc8ee9187c1d7230def4d665f7dd3948e6983
kuwood <p4nic510@gmail.com>
2016-09-12 14:39:34 -0600

    Fix spelling error in define section, remove contact section and add email in social links

--- js/app.js
+++ js/app.js
@@ -22,50 +22,29 @@ function showProjects() {
     });
 }
 
-function showContact() {
-  $('.main-content .content-wrap').children().hide();
-  $('.contact').fadeIn();
-  $('.content-wrap').css({"display":"table"});
-  $('.main-content').css({"padding-left":"0"});
-  $('.main-content').css({"width":"50%"});
-}
-
 function createNav() {
   var content = ["Keith Underwood",
-                "What I've done",
-                "Contact me"];
+                "Projects"];
   content.forEach(function(i) {
     var liItem = $("<li><a id='"+content.indexOf(i) +
                     "' class='nav-link'>"+i+"</a></li>");
     $('#nav-list').append(liItem);
     liItem.click(function() {
       navRouter(liItem);
-    });
+  });
   });
 }
 
 function navRouter(i) {
-  if(i.text() === "Keith Underwood") {
+  if (i.text() === "Keith Underwood") {
     $('.nav-link').animate({'font-size':'1.2em'}, 50);
     $('#0').animate({'font-size':'1.4em'}, 100);
     showDefine();
-  } else if (i.text() === "What I've done") {
+} else {
     $('.nav-link').animate({'font-size':'1.2em'}, 50);
     $('#1').animate({'font-size':'1.4em'}, 100);
     showProjects();
-  } else {
-    $('.nav-link').animate({'font-size':'1.2em'}, 50);
-    $('#2').animate({'font-size':'1.4em'}, 100);
-    showContact();
   }
-}
-
-function sendMail() {
-  var link = "mailto:keithunderwood510@gmail.com?" +
-               "subject=" + $('#name-text').val() + " - " +
-               $('#subject-text').val().toString() +
-               "&body=" + $('#body-text').val().toString();
-  window.location.href = link;
 }
 
 $(function() {
