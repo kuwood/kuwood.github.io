44224dcad2af8c9a33b3a74acdc42a1a50ddd3fc
Keith Underwood <keithunderwood510@gmail.com>
2016-11-26 10:49:15 -0800

    Fix animation stutter, update location in description

--- js/app.js
+++ js/app.js
@@ -1,58 +1,63 @@
 function showDefine() {
-    $('.main-content .content-wrap').children().hide();
-    $('.define').fadeIn();
-    $('.content-wrap').css({
-        "display": "flex"
-    });
-    $('#0').css({
-        'font-size': '1.4em'
-    });
+  $('.main-content .content-wrap').children().hide();
+  $('.define').fadeIn();
+  $('.content-wrap').css({
+    "display": "flex"
+  });
 }
 
 function showProjects() {
-    $('.main-content .content-wrap').children().hide();
-    $('.content-wrap').css({
-        "display": "flex"
-    });
-    $('.projects-wrap').fadeIn();
+  $('.main-content .content-wrap').children().hide();
+  $('.content-wrap').css({
+    "display": "flex"
+  });
+  $('.projects-wrap').fadeIn();
 }
 
+// creates li for each nav item
 function createNav() {
-    var content = ["Keith Underwood",
-        "Projects"
-    ];
-    content.forEach(function(i) {
-        var liItem = $("<li><a id='" + content.indexOf(i) +
-            "' class='nav-link'>" + i + "</a></li>");
-        $('.nav-list').append(liItem);
-        liItem.click(function() {
-            navRouter(liItem);
-        });
+  var content = ["Keith Underwood", "Projects"];
+  content.forEach(function(i) {
+    var liItem = $("<li><a id='" + content.indexOf(i) +
+      "' class='nav-link'>" + i + "</a></li>");
+    $('.nav-list').append(liItem);
+    liItem.click(function() {
+      navRouter(liItem);
     });
+  });
 }
 
+// animates and handles show/hide of content
 function navRouter(i) {
-    if (i.text() === "Keith Underwood") {
-        $('.nav-link').animate({
-            'font-size': '1.2em'
-        }, 50);
-        $('#0').animate({
-            'font-size': '1.4em'
-        }, 100);
-        showDefine();
-    } else {
-        $('.nav-link').animate({
-            'font-size': '1.2em'
-        }, 50);
-        $('#1').animate({
-            'font-size': '1.4em'
-        }, 100);
-        showProjects();
-    }
+  if (i.text() === "Keith Underwood") {
+    navAnimation('#0')
+    showDefine();
+  } else {
+    navAnimation('#1')
+    showProjects();
+  }
+}
+
+function navAnimation(selection) {
+  if (selection === '#0') {
+    $('#0').animate({
+      'font-size': '1.4em'
+    }, 100)
+    $('#1').animate({
+      'font-size': '1.2em'
+    }, 100)
+  } else {
+    $('#1').animate({
+      'font-size': '1.4em'
+    }, 100)
+    $('#0').animate({
+      'font-size': '1.2em'
+    }, 100)
+  }
 }
 
 $(function() {
-
-    createNav();
-    showDefine();
+  createNav();
+  navAnimation('#0')
+  showDefine();
 });
